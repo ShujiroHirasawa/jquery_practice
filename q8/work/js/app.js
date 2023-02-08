ca82d86fe38d1bdef9a4850344930cc011b04c36
shujiro <shujiro914@outlook.jp>
2023-02-08 22:11:55 +0900

    3rd commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,20 +1,18 @@
 //APIを使用した実装//
 $(function(){
-  $('.search-btn').on("click",function(){
-    $.ajax({
-      url: "https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20",
-      method: "GET",
-    }).done(function(response){
-      //成功したときの処理//
-      const result=response['@graph'];
-      displayResult(result)
-    }).fail(function(err){
-      //失敗したときの処理//
-      displayError(err)
-    });
-  })
+  const settings = {
+  url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+  method: "GET",
+}
+$('.search-btn').on("click",function(){
+  $.ajax(settings).done(function (response) {
+  const result = response['@graph'];
+  displayResult(result)
+}).fail(function (err) {
+  displayError(err)
 });
-
+})
+});
 
 
 
@@ -29,7 +27,7 @@ $(function(){
   "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
   "method": "GET",
 }
-$('.search-btn btn').click(function(){
+$('.search-btn').on("click",function(){
   $.ajax(settings).done(function (response) {
   const result = response['@graph'];
   displayResult(result)
