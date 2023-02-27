101166b917b0af60bf6b57f65831e236b8f77527
shujiro <shujiro914@outlook.jp>
2023-02-27 23:17:11 +0900

    5th commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,22 +1,26 @@
 //APIを使用した実装//
 $(function(){
-  const settings = {
-  url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-  method: "GET",
-}
-$(".search-btn").on("click",function(){
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    const result = response['@graph'];
-    displayResult(result)
-}).fail(function (response) {
-  displayError(response)
-});
-$(".reset.btn").on("click",function(){
-  $(".lists").empty();
-})
-})
-});
+  const searchWord="図鑑";
+  const pageCount=1;
+  $(".search-btn").on("click",function(){
+    const settings = {
+      url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+      method: "GET",
+    }
+    $.ajax(settings).done(function (response) {
+      //const settings="";//
+      console.log(settings);
+      const result = response['@graph'];
+      displayResult(result)
+    }).fail(function (response) {
+      displayError(response)
+    });
+  $(".reset.btn").on("click",function(){
+    $(".lists").empty();
+  })
+  })
+  });
+
 
 
 
